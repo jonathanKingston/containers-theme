b50a55c7d79d4ca3c9dd3f634aadc855b6d32611
Jonathan Kingston <jonathan@jooped.co.uk>
2017-09-20 21:54:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Containers theme
+
+Changes the current browser window theme to match the colour of the active tab.
+
+![Screnshot showing different themes](img/screenshot.png)
---
+++ background.js
@@ -0,0 +1,69 @@
+class containersTheme {
+  constructor() {
+    browser.tabs.onActivated.addListener(() => {
+      this.getCurrentContainer();
+    });
+    browser.windows.onFocusChanged.addListener(() => {
+      this.getCurrentContainer();
+    });
+    this.getCurrentContainer();
+  }
+
+  async getCurrentContainer() {
+    const activeTabs = await browser.tabs.query({
+      active: true
+    });
+    const hasUnpainted = activeTabs.some((tab) => {
+      return this.isUnpaintedTheme(tab.cookieStoreId);
+    });
+    const containers = await this.getContainers();
+    if (hasUnpainted) {
+      this.resetTheme();
+    }
+    activeTabs.forEach((tab) => {
+      const cookieStoreId = tab.cookieStoreId;
+      if (!this.isUnpaintedTheme(cookieStoreId)) {
+        this.changeTheme(cookieStoreId,
+          tab.windowId,
+          containers.get(cookieStoreId));
+      }
+    });
+  }
+
+  async getContainers() {
+    const containersMap = new Map();
+    const containers = await browser.contextualIdentities.query({});
+    containers.forEach((container) => {
+      containersMap.set(container.cookieStoreId, container);
+    });
+    return containersMap;
+  }
+
+  isUnpaintedTheme(currentCookieStore) {
+    return (currentCookieStore == "firefox-default" ||
+            currentCookieStore == "firefox-private");
+  }
+
+  resetTheme() {
+    // Because of the following, we loop through all active windows after a reset
+    // this means when we have unpained tabs the browser flickers
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1401691
+    browser.theme.reset();
+  }
+
+  async changeTheme(currentCookieStore, windowId, container) {
+    this.cachedCookieStore = currentCookieStore;
+    return browser.theme.update(windowId, {
+      images: {
+        headerURL: "none",
+      },
+      colors: {
+        accentcolor: container.colorCode,
+        textcolor: "#111",
+      }
+    });
+  }
+
+}
+
+new containersTheme();
